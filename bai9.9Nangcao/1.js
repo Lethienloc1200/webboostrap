4af72c9d0f28249babd1ebfda16f826bf99db199
Your Name <you@example.com>
2020-09-04 19:47:26 +0700

    them file web

---
+++ bai9.9Nangcao/1.js
@@ -0,0 +1,13 @@
+﻿$(function(){
+	$(window).scroll(function(){
+
+		if($('body').scrollTop()>100){
+			$('.logo').addClass('chucam');
+			$('nutlen').addClass('hienthi');
+		}
+		else if('body'.scrollTop()<=100)
+		{
+			$('.logo').removeClass('chucam');
+		}
+	})
+})
